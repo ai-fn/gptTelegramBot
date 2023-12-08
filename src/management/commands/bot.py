4099d3bdfaf3ee30a77eb3cc33a1cbb283517d1e
zhirov <zhirov_andrei02@list.ru>
2023-12-08 23:58:06 +0300

    some refactoring

--- src/management/commands/bot.py
+++ src/management/commands/bot.py
@@ -1,17 +1,17 @@
 import os
-import logging
 
 import g4f
+import logging
 
 from django.core.management.base import BaseCommand
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 from telegram import Update, User, Bot, ChatAction, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import Updater, Filters, MessageHandler, CommandHandler, CallbackQueryHandler
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
 from src.models import Message, Profile
 
-at = os.getenv("BOT_API_TOKEN")
 logger = logging.getLogger()
+at = os.getenv("BOT_API_TOKEN")
 updater = Updater(token=at, use_context=True)
 
 
@@ -20,7 +20,7 @@ class Command(BaseCommand):
 
     def __init__(self):
         super().__init__()
-        self.dev = Profile.objects.first()
+        self.dev = Profile.objects.first()  # assuming that you(dev) are first user
         self.developer = User(
             id=self.dev.tg_id,
             first_name=self.dev.first_name,
@@ -45,31 +45,45 @@ class Command(BaseCommand):
         updater.start_polling(allowed_updates=Update.ALL_TYPES)
 
     def handle(self, *args, **options):
-        """Handle the incoming message"""
+        """Handle the incoming messages"""
+
         message_handler = MessageHandler(Filters.text, self.handle_message)
         updater.dispatcher.add_handler(message_handler)
 
     @staticmethod
     def help_command(update: Update, context) -> None:
         """Displays info on how to use the bot."""
+
         update.message.reply_text(
             "Use /start to test this bot. "
-            "Allowed commands:\n/start - to start bot\n"
-            "/help - to get more info about this bot\n/change_model - to change language model"
+            "Allowed commands:\n"
+            "/start - to start bot\n"
+            "/help - to get more info about this bot\n"
+            "/change_model - to change language model"
         )
 
     def start_command(self, update: Update, context) -> None:
         """Displays info on how to use the bot."""
+
+        menu_main = [
+            [InlineKeyboardButton('Option 1', callback_data='m1')],
+            [InlineKeyboardButton('Option 2', callback_data='m2')],
+            [InlineKeyboardButton('Option 3', callback_data='m3')]
+        ]
+        reply_markup = InlineKeyboardMarkup(menu_main)
+
         update.message.reply_text(
             f"I`m a (beta) {self.model_name} language model. Send me any sentence (best in English) "
             f"and i will try to answer\n"
             f"For example, send me 'Who is your creator?'\n"
             f"Use /help to get all info\n"
-            f"If generating response are failed, try to change model by /change_model command"
+            f"If generating response are failed, try to change model by /change_model command",
+            reply_markup=reply_markup
         )
 
     def change_model_command(self, update: Update, context) -> None:
         """Displays info on how to change language model type of the bot."""
+
         buttons = [[InlineKeyboardButton(text=model, callback_data=model) for model in self.name_map]]
         keyboard = InlineKeyboardMarkup(buttons)
         update.message.reply_text(
@@ -87,6 +101,7 @@ class Command(BaseCommand):
 
     def button(self, update: Update, context) -> None:
         """Parses the CallbackQuery and updates the message text."""
+
         query = update.callback_query
 
         self.model = self.get_from_old_name(query.data)
@@ -96,6 +111,7 @@ class Command(BaseCommand):
 
     def handle_message(self, update: Update, context):
         """Handle the incoming message"""
+
         sender = update.message.from_user
         if Profile.objects.filter(name=sender.username).exists():
             user = Profile.objects.get(name=sender.username)
@@ -112,21 +128,21 @@ class Command(BaseCommand):
         text = message.text
         resp = update.message.reply_text(text="-Generating response...")
         self.bot.send_chat_action(chat_id=chat_id, action=ChatAction.TYPING)
+
         response = self.generate_response(text)
-        if "OpenAI" in response:
-            response = response.replace("OpenAI", f"@{self.developer.username}")
-        elif "Phind" in response:
-            response = response.replace("Phind", f"@{self.developer.username}")
-        resp.delete()
-        self.bot.sendMessage(chat_id=chat_id, text=response)
-        if update.message.from_user.first_name != self.developer.first_name:
+        msg_id = message.message_id
+        self.bot.sendMessage(chat_id=chat_id, text=response, reply_to_message_id=msg_id)
+        if message.from_user.first_name != self.developer.first_name:
             self.bot.sendMessage(
-                chat_id=self.dev.tg_id, text=f"User {user.first_name} sent message '{text}' and got response '{response}'"
+                chat_id=self.dev.tg_id,
+                text=f"User {user.first_name} sent message '{text}' and got response '{response}'"
             )
         Message.objects.create(sender=user, body=text, response=response).save()
+        resp.delete()
 
-    def generate_response(self, input_text):
+    def generate_response(self, input_text: str) -> str:
         """Generation response by the setting language model"""
+
         if not isinstance(self.model, g4f.models.Model):
             input_ids = self.tokenizer.encode(input_text, return_tensors="pt")
             output = self.model.generate(input_ids, max_length=100, num_return_sequences=1)
@@ -139,5 +155,11 @@ class Command(BaseCommand):
                 )
             except RuntimeError as err:
                 logger.error(err)
-                response = "😕 Sorry, too many people have been asking me questions at once. Give me a moment and try again."
+                response = ("😕 Sorry, too many people have been asking me questions at once."
+                            " Give me a moment and try again.")
+        if "OpenAI" in response:
+            response = response.replace("OpenAI", f"@{self.developer.username}")
+        elif "Phind" in response:
+            response = response.replace("Phind", f"@{self.developer.username}")
+
         return response
